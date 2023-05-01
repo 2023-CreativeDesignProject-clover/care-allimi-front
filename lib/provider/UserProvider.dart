d87c9c5c76e9637cffe888ac5afac69a4e5c2646
hyorimmmm <hyorim0930@naver.com>
2023-05-01 16:04:09 +0900

    커밋

---
+++ lib/provider/UserProvider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class UserProvider with ChangeNotifier {
+  int uid = 0;
+  String urole ='';
+  String loginid ='';
+  String phone_num = '';
+  String name = '';
+
+  void setInfo(int uid, String urole, String loginid, String phone_num, String name) {
+    this.uid = uid;
+    this.urole = urole;
+    this.loginid = loginid;
+    this.phone_num = phone_num;
+    this.name = name;
+
+    notifyListeners();
+  }
+
+  void getData() {
+    notifyListeners();
+  }
+
+  void changeRoleData() {
+    this.urole = 'asdf';
+    notifyListeners();
+  }
+
+  void logout() {
+    this.uid = 0;
+    this.urole = '';
+    this.loginid = '';
+    this.phone_num = '';
+    this.name = '';
+
+    notifyListeners();
+  }
+
+//TODO 로그아웃
+}
